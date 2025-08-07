9d84c18b645487ae70b97124de84a97bef78bcdf
ahmed hachicha <ahmedhachicha20@gmail.com>
2025-08-07 10:54:32 +0100

    change pipeline

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,22 @@
 module.exports = {
-    apps: [
-        {
-            name: "my-app",
-            script: "npm",
-            args: "start",
-            env: {
-                port:4000,
-                NODE_ENV: "production"
-            }
-        }
-    ]
+  apps: [
+    {
+      name: 'vue-cicd-demo',
+      script: 'npm',
+      args: 'run dev',
+      cwd: './',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 4000
+      },
+      error_file: './logs/vue-cicd-demo-error.log',
+      out_file: './logs/vue-cicd-demo-out.log',
+      log_file: './logs/vue-cicd-demo-combined.log',
+      time: true
+    }
+  ],
 };
